cbf4f0ecf0a56d8e5117ca3ae5d3c1121e011f26
Tuhaj <zrazic@wp.pl>
2017-03-05 17:16:15 +0100

    adds condition on modal display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  modalVisible = true
   watchVideo(event) {
     console.log(event, 'in app component');
   }
